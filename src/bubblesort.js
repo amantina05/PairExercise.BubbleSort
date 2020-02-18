2d35566cb5c82cda0362b8afc69dd1a12e8b4afd
Amantina Sosa <amantinasosa@gmail.com>
2020-02-18 14:57:24 -0500

    updated

--- src/bubblesort.js
+++ src/bubblesort.js
@@ -1,3 +1,19 @@
-function bubbleSort() {
-  return [1, 2, 3];
+function bubbleSort(array, index = array.length - 1, comparator) {
+  let i = 0;
+  let j = 1;
+
+  if (index === 0) return array;
+
+  while (index >= j) {
+    if (comparator(array[i], array[j]) === 1) {
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    i += 1;
+    j += 1;
+  }
+
+  return bubbleSort(array, index - 1, comparator);
 }
+
+module.exports = bubbleSort;
--- test/bubblesort.spec.js
+++ test/bubblesort.spec.js
@@ -1,10 +1,18 @@
 const { expect } = require('chai');
+const bubbleSort = require('../src/bubblesort');
 
 const unsorted = [2, 4, 5, 7, 3, 8, 6, 1];
 const sorted = [1, 2, 3, 4, 5, 6, 7, 8];
+const sortedInc = [8, 7, 6, 5, 4, 3, 2, 1];
 
 describe('BubbleSort', () => {
   it('should sort an unsorted array', () => {
-    expect(bubbleSort(unsorted)).to.be(sorted);
+    expect(
+      bubbleSort(unsorted, unsorted.length - 1, (i, j) => {
+        if (i < j) return 1;
+        if (i > j) return -1;
+        return 0;
+      })
+    ).to.be.deep.equal(sortedInc);
   });
 });
