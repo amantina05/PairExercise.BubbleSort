7a9013f88b5a82d6b8154f4c4fafdb4ac6452c0e
Amantina Sosa <amantinasosa@gmail.com>
2020-02-19 17:23:49 -0500

    complete

--- src/mergesort.js
+++ src/mergesort.js
@@ -8,25 +8,24 @@ function split(wholeArray) {
 }
 
 function lt(a, b) {
-  return a < b;
+  return a < b ? -1 : 1;
 }
 
 function merge(array1, array2, comparator = lt) {
   let i = 0;
   let j = 0;
   const mergedArr = [];
+
   while (array1[i] && array2[j]) {
-    console.log('comparator', comparator(array1[i], array2[j]));
-    console.log('array1', array1[i], 'array2', array2[j]);
-    if (comparator(array1[i], array2[j])) {
+    if (comparator(array1[i], array2[j]) === -1) {
       mergedArr.push(array1[i]);
       i++;
     } else {
-      // } else if (comparator(array2[j], array1[i])) {
       mergedArr.push(array2[j]);
       j++;
     }
   }
+
   while (array1[i]) {
     mergedArr.push(array1[i]);
     i++;
@@ -35,22 +34,18 @@ function merge(array1, array2, comparator = lt) {
     mergedArr.push(array2[j]);
     j++;
   }
-  console.log(mergedArr);
   return mergedArr;
 }
 
 function mergeSort(array, comparator = lt) {
-  if (array.length === 1) {
+  if (array.length <= 1) {
     return array;
   }
 
   let [left, right] = split(array);
-
   left = mergeSort(left, comparator);
   right = mergeSort(right, comparator);
   let merged = merge(left, right, comparator);
-  console.log(merged);
   return merged;
 }
-
 module.exports = { split, merge, mergeSort };
